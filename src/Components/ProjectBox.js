4fe8101c597eaed79718b7cd795dcc471cb9af5e
Tarik Madi <maditarik56@gmail.com>
2025-05-27 11:10:32 +0200

    ajout de projet

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -1,13 +1,16 @@
-import React from 'react';
 import {FaGithub} from "react-icons/fa";
 import {CgFileDocument} from "react-icons/cg";
 
 
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
-    quizzDesc:"un quizz Javascript sur le thème de la programmation",
-    quizzGithub:"https://github.com/MenChad/quizz",
-    quizzWebsite:"https://menchad.github.io/quizz",
+    freelanceDesc:"un site vitrine pour un freelance en développement web avec traduction",
+    freelanceGithub:"https://github.com/MenChad/freelance",
+    freelanceWebsite:"https://freelance-o7g3.vercel.app/fr",
+
+    rando_sudDesc:"un site vitrine pour une agence de randonnée dans le sud de la France",
+    rando_sudGithub:"https://github.com/MenChad/rando_sud",
+    rando_sudWebsite:"https://menchad.github.io/rando_sud/",
 
     plat_decouverteDesc:"une agence de voyage designée en html et css ",
     plat_decouverteGithub:"https://github.com/MenChad/plat_decouverte",
@@ -16,6 +19,10 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
     clone_morpion_reactDesc:"un clone du jeu morpion en react",
     clone_morpion_reactGithub:"https://github.com/MenChad/clone_morpion_react",
     clone_morpion_reactWebsite:"https://clone-morpion-react.vercel.app/",
+
+    quizzDesc:"un quizz Javascript sur le thème de la programmation",
+    quizzGithub:"https://github.com/MenChad/quizz",
+    quizzWebsite:"https://menchad.github.io/quizz",
 
     
 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,17 +1,21 @@
-import React from 'react';
 import ProjectBox from './ProjectBox';
 import quizzImage from '../images/quizzImage.png';
 import cloneMorpionImage from '../images/cloneMorpionImage.png';
 import platDecouverteImage from '../images/platDecouverteImage.png';
+import freelanceImage from '../images/freelanceImage.png';
+import randoSudImage from '../images/randoSudImage.png';
 
 const Projects = () => {
   return (
     <div>
       <h1 className='projectHeading'>Mes <b>Projets</b></h1>
       <div className='project'>
-        <ProjectBox projectPhoto={quizzImage} projectName="quizz" />
+        <ProjectBox projectPhoto={freelanceImage} projectName="freelance" />
+        <ProjectBox projectPhoto={randoSudImage} projectName="rando_sud" />
         <ProjectBox projectPhoto={platDecouverteImage} projectName="plat_decouverte" />
         <ProjectBox projectPhoto={cloneMorpionImage} projectName="clone_morpion_react" />
+        <ProjectBox projectPhoto={quizzImage} projectName="quizz" />
+
       </div>
 
     </div>
